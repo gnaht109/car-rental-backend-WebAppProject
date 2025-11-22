66b8d313b1d362213767a0a5a1f651ca933a23ae
gnaht109 <94453695+gnaht109@users.noreply.github.com>
2025-11-23 00:24:36 +0700

    UserController.java

--- src/main/java/com/car_rental_backend/Controller/UserController.java
+++ src/main/java/com/car_rental_backend/Controller/UserController.java
@@ -1,7 +1,47 @@
 package com.car_rental_backend.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.car_rental_backend.Service.UserService;
+import com.car_rental_backend.Model.User;
+
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000") // Adjust the origin as needed
 public class UserController {
-    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //LogIn endpoint
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        User u = userService.login(user.getUsername(), user.getPassword());
+        if (u == null) {
+            return "LOGIN FAIL";
+        }
+        return "LOGIN SUCCESS" + user.getUsername();
+    }
+
+    //SignUp endpoint
+    @PostMapping("/signup")
+    public String signUp(@RequestBody User user) {
+        User u = userService.signUp(
+            user.getUsername(),
+            user.getPassword(), 
+            user.getRole(), 
+            user.getEmail(), 
+            user.getPhone()
+        );
+        if (u == null) {
+            return "REGISTER FAIL";
+        }
+        return "SIGNUP SUCCESS" + user.getUsername();
+    }
 }
